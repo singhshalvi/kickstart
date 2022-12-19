9a984b274e9f970a34cf9956dc22e886bd9a2c52
singhshalvi <71918488+singhshalvi@users.noreply.github.com>
2022-12-19 11:40:00 +0530

    Add files via upload

---
+++ kickstart/ethereum/deploy.js
@@ -0,0 +1,26 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 =require('web3');
+// const { interface, bytecode }= require('./compile');
+const compiledFactory = require('./build/CampaignFactory.json');
+
+const provider = new HDWalletProvider(
+    'panther room polar stand guide staff battle give result public occur network',
+    'https://goerli.infura.io/v3/d979eb0767c14c719a6eb5ebbae0a608'
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async() => {
+const accounts =await web3.eth.getAccounts();
+
+console.log('Attempting to deploy from account', accounts[0]);
+
+const result = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
+.deploy({ data: compiledFactory.bytecode})
+.send({ gas: '1000000', from: accounts[0]});
+// console.log(interface);
+console.log('Contract deployed to', result.options.address);
+provider.engine.stop();
+};
+
+deploy();
